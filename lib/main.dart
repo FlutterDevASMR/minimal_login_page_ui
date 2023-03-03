e74b893c040e1da41ce3fe2a8546bc91f1c5d81e
FlutterDevASMR <flutterdevasmr@gmail.com>
2023-03-03 11:48:45 +0330

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,178 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MinimalLoginPageUI());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MinimalLoginPageUI extends StatelessWidget {
+  const MinimalLoginPageUI({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool rememberMe = false;
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.deepOrange,
+                  Colors.deepPurple,
+                ],
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              height: height * 0.57,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    const Icon(
+                      Icons.login,
+                      size: 70,
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Sign In',
+                      style:
+                          TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 20),
+                    MyTextField(
+                      hintText: 'Email',
+                      isPassword: false,
+                      prefixIcon: const Icon(
+                        Icons.email,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    MyTextField(
+                      hintText: 'Password',
+                      isPassword: true,
+                      prefixIcon: const Icon(
+                        Icons.password,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Checkbox(
+                          activeColor: Colors.black,
+                          value: rememberMe,
+                          onChanged: (value) {
+                            setState(() {
+                              rememberMe = value!;
+                            });
+                          },
+                        ),
+                        const Text('Remember Me'),
+                        const Spacer(),
+                        Text(
+                          'Forgot password',
+                          style: TextStyle(
+                            color: Colors.blue.shade900,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        backgroundColor: Colors.black,
+                        minimumSize: const Size(double.infinity, 40),
+                      ),
+                      child: const Text(
+                        'Sign In',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text('Don\'t have an account? '),
+                        Text(
+                          'Sign Up',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  MyTextField(
+      {super.key,
+      required this.hintText,
+      required this.isPassword,
+      required this.prefixIcon});
+
+  String hintText;
+  bool isPassword;
+  Widget prefixIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40,
+      child: TextField(
+        style: const TextStyle(fontSize: 14),
+        obscureText: isPassword,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white.withOpacity(0.5),
+          prefixIcon: prefixIcon,
+          hintText: hintText,
+          contentPadding: const EdgeInsets.only(top: 2),
+          hintStyle: const TextStyle(fontSize: 14),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide.none),
+        ),
+      ),
     );
   }
 }
